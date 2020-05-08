d097a5b968ab9e3310e0b4eca86c0d430d9d0d66
Han Deng <han.deng@outlook.com>
2020-05-07 21:41:56 -0400

    add git ignore and projection code

---
+++ projectPoints/test.sh
@@ -0,0 +1,4 @@
+python proj_pseudo_cp_image.py --image_dir=/home/han/gitlab/pc_pro/dataset/2014-07-14-14-49-50/stereo/left \
+			       --models_dir=/home/han/gitlab/pc_pro/dataset/camera-models/ \
+			       --points_history_file=/home/han/gitlab/pc_pro/dataset/2014-07-14-14-49-50/pts_history_file.txt \
+			       --pose_history_file=/home/han/gitlab/pc_pro/dataset/2014-07-14-14-49-50/poses_history_file.txt
